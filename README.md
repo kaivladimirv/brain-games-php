1ff12d0a4e6cb089d728bd223d42598044c9a640
kaivladimirv <kaivladimirv@gmail.com>
2022-08-11 13:36:24 +0600

    feat: Create EvenGame

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/kaivladimirv/php-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/kaivladimirv/php-project-lvl1/actions)
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/6a85cca81fd5662c8c25/maintainability)](https://codeclimate.com/github/kaivladimirv/php-project-lvl1/maintainability)
+
+[![asciicast](https://asciinema.org/a/3Cl8LBmMF7Lwex2ekKQxzamRr.svg)](https://asciinema.org/a/3Cl8LBmMF7Lwex2ekKQxzamRr)
---
+++ src/EvenGame.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BrainGames\EvenGame;
+
+use function cli\line;
+use function cli\prompt;
+
+function run(): void
+{
+    line("\nWelcome to the Brain Games!");
+
+    $userName = prompt('May I have your name?', '', ' ');
+    line("Hello, $userName!");
+
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+
+    foreach (getEvenNumbers() as $number) {
+        line("Question: $number");
+
+        $expectedAnswer = ($number % 2 === 0 ? 'yes' : 'no');
+        $receivedAnswer = prompt('Your answer');
+
+        if ($expectedAnswer === $receivedAnswer) {
+            line('Correct!');
+        } else {
+            line("'$receivedAnswer' is wrong answer ;(. Correct answer was '$expectedAnswer'.");
+            line("Let's try again, $userName!");
+            exit;
+        }
+    }
+
+    line("Congratulations, $userName!");
+}
+
+function getEvenNumbers(): array
+{
+    return [
+        15,
+        6,
+        7,
+    ];
+}
