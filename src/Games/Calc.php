2581a8c3c941581ef391ffb6a3118b3f8d4777b5
kaivladimirv <kaivladimirv@gmail.com>
2022-08-22 10:00:17 +0600

    refactor(calc): Remove default value

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -69,12 +69,11 @@ function getRandomOperation(): string
     return $operations[$index];
 }
 
-function calcExpectedAnswer(string $operation, array $numberPair): ?int
+function calcExpectedAnswer(string $operation, array $numberPair): int
 {
     return match ($operation) {
         '+' => $numberPair[0] + $numberPair[1],
         '-' => $numberPair[0] - $numberPair[1],
-        '*' => $numberPair[0] * $numberPair[1],
-        default => null,
+        '*' => $numberPair[0] * $numberPair[1]
     };
 }
